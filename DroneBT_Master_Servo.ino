b96a9c44d695724a839cdc6c3acf084c5d82cbb0
MichaelYuan <yuan7407@gmail.com>
2017-06-21 20:43:42 +0800

    The Arduino Part

---
+++ DroneBT_Master_Servo.ino
@@ -0,0 +1,15 @@
+const int buttonPin = 2;
+
+int buttonState = 0;
+void setup() {
+  pinMode(buttonPin, INPUT);
+  Serial.begin(38400);
+}
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    Serial.write('1');
+  }else{
+    Serial.write('0');
+  }
+}
---
+++ DroneBT_Slave_Servo.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+Servo myservo;
+int pos = 180;
+
+int buttonState = 0;
+
+int val = 9999;
+void setup() {
+  myservo.attach(9);
+  Serial.begin(38400);
+}
+
+void loop() {
+  val = Serial.read();
+  Serial.println(val);
+  if (Serial.read() == '1') {/*
+    for (pos = 0; pos < 180; pos += 1) {
+      myservo.write(pos);
+      delay(5);
+    }
+*/
+/*
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }*/
+    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+
+  }
+
+  if (Serial.read() == '0') {
+    //pos = 179;
+    //myservo.write(pos);
+    }
+  //delay(100);
+}
